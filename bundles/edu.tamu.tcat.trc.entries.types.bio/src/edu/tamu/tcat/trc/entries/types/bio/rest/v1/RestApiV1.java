8c69ac6b9cb533d396ebbc21f70360750ed076f1
Paul Bilnoski <bilnoski@tamu.edu>
2015-05-23 10:39:31 -0500

    Collect REST API into a semantic space

--- bundles/edu.tamu.tcat.trc.entries.types.bio/src/edu/tamu/tcat/trc/entries/types/bio/rest/v1/RestApiV1.java
+++ bundles/edu.tamu.tcat.trc.entries.types.bio/src/edu/tamu/tcat/trc/entries/types/bio/rest/v1/RestApiV1.java
@@ -1,10 +1,65 @@
 package edu.tamu.tcat.trc.entries.types.bio.rest.v1;
 
+import java.util.Date;
+import java.util.Set;
+
 /**
  * An encapsulation of the data vehicle types used to process JSON requests and responses
  * for version 1 of the TRC REST API for Persons.
  */
 public class RestApiV1
 {
+   public static class PersonId
+   {
+      public String id;
+   }
+
+   /**
+    * Represents a Person
+    */
+   public static class Person
+   {
+      public String id;
+      public PersonName displayName;
+      public Set<PersonName> names;
+      public HistoricalEvent birth;
+      public HistoricalEvent death;
+      public String summary;
+   }
    
+   public static class PersonName
+   {
+      public String title;
+      public String givenName;
+      public String middleName;
+      public String familyName;
+      public String suffix;
+
+      public String displayName;
+   }
+   
+   public static class HistoricalEvent
+   {
+
+      public String id;
+      public String title;
+      public String description;
+      public String location;
+
+      /** The date this event took place. */
+      public DateDescription date;
+
+      /** Replaced by date. */
+      @Deprecated
+      public Date eventDate;
+   }
+   
+   public static class DateDescription
+   {
+      /** ISO 8601 local (YYYY-MM-DD) representation of this date. */
+      public String calendar;
+
+      /** A human readable description of this date. */
+      public String description;
+   }
 }
