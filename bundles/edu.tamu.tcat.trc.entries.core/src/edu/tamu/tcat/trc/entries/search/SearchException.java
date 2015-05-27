62d6798fa0e7a69b457706b181fd4492334fce9f
Paul Bilnoski <bilnoski@tamu.edu>
2015-05-26 21:10:34 -0500

    Add exception for general use

---
+++ bundles/edu.tamu.tcat.trc.entries.core/src/edu/tamu/tcat/trc/entries/search/SearchException.java
@@ -0,0 +1,31 @@
+package edu.tamu.tcat.trc.entries.search;
+
+/**
+ * A base exception type for entry types to use within their search modules.
+ */
+public class SearchException extends Exception
+{
+   public SearchException()
+   {
+   }
+
+   public SearchException(String message)
+   {
+      super(message);
+   }
+
+   public SearchException(Throwable cause)
+   {
+      super(cause);
+   }
+
+   public SearchException(String message, Throwable cause)
+   {
+      super(message, cause);
+   }
+
+   public SearchException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+   {
+      super(message, cause, enableSuppression, writableStackTrace);
+   }
+}
