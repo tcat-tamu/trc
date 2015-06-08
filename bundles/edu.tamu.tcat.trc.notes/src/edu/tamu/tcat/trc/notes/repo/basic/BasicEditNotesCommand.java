5b526e6410a8843c82d9f64a68499c2c18adf709
Jesse Mitchell <jessemitchell@tees.tamus.edu>
2015-06-08 10:25:52 -0500

    Change class to abstract and added update and isNew methods.

--- bundles/edu.tamu.tcat.trc.notes/src/edu/tamu/tcat/trc/notes/repo/basic/BasicEditNotesCommand.java
+++ bundles/edu.tamu.tcat.trc.notes/src/edu/tamu/tcat/trc/notes/repo/basic/BasicEditNotesCommand.java
@@ -1,13 +1,14 @@
 package edu.tamu.tcat.trc.notes.repo.basic;
 
 import java.net.URI;
+import java.util.Objects;
 import java.util.UUID;
 
 import edu.tamu.tcat.trc.notes.Notes;
 import edu.tamu.tcat.trc.notes.dto.NotesDTO;
 import edu.tamu.tcat.trc.notes.repo.EditNotesCommand;
 
-public class BasicEditNotesCommand implements EditNotesCommand
+public abstract class BasicEditNotesCommand implements EditNotesCommand
 {
 
    protected final Notes original;
@@ -24,6 +25,32 @@ public class BasicEditNotesCommand implements EditNotesCommand
    {
       this.original = NotesDTO.instantiate(dto);
       this.dto = NotesDTO.copy(dto);
+   }
+
+   @Override
+   public final void update(NotesDTO updateDTO)
+   {
+      if (!isNew())
+      {
+         if(dto.id != null && updateDTO.id != null & !Objects.equals(dto.id, updateDTO.id))
+            throw new IllegalArgumentException("Invalid update: identifiers do not match. "
+                  + "Current [" + dto.id + "]. Updates [" + updateDTO.id + "]");
+      }
+
+      if (dto.id == null)
+         dto.id = updateDTO.id;
+
+      if (updateDTO.associatedEntity != null)
+         setEntity(updateDTO.associatedEntity);
+
+      if (updateDTO.authorId != null)
+         setAuthorId(updateDTO.authorId);
+
+      if (updateDTO.mimeType != null)
+         setMimeType(updateDTO.mimeType);
+
+      if (updateDTO.content != null)
+         setContent(updateDTO.content);
    }
 
    @Override
@@ -60,4 +87,8 @@ public class BasicEditNotesCommand implements EditNotesCommand
       return this;
    }
 
+   protected final boolean isNew()
+   {
+      return original == null;
+   }
 }
