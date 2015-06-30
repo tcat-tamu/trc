2328773e75a2958b8500e1f0c2e05d0f15be662a
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-06-30 11:51:38 -0500

    Config object for page-level full text search
    https://issues.citd.tamu.edu/browse/CE-113

---
+++ bundles/edu.tamu.tcat.trc.entries.types.bib/src/edu/tamu/tcat/trc/entries/types/bib/copies/search/solr/FullTextPageConfig.java
@@ -0,0 +1,73 @@
+package edu.tamu.tcat.trc.entries.types.bib.copies.search.solr;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.apache.solr.client.solrj.SolrQuery;
+
+import edu.tamu.tcat.trc.entries.search.SearchException;
+import edu.tamu.tcat.trc.entries.search.solr.SolrIndexConfig;
+import edu.tamu.tcat.trc.entries.search.solr.SolrIndexField;
+import edu.tamu.tcat.trc.entries.search.solr.impl.BasicFields;
+import edu.tamu.tcat.trc.entries.types.bib.copies.search.PageSearchProxy;
+
+/**
+ * Defines the Solr configuration for indexing full text search results on a page level.
+ */
+public class FullTextPageConfig implements SolrIndexConfig
+{
+
+   public static final SolrIndexField<String> ID = new BasicFields.BasicString("id");
+   private final static SolrIndexField<String> TEXT = new BasicFields.BasicString("pageText");
+   private final static SolrIndexField<Integer> NUMBER = new BasicFields.BasicInteger("pageNumber");
+   private final static SolrIndexField<String> SEQUENCE = new BasicFields.BasicString("pageSequence");
+
+
+   public FullTextPageConfig()
+   {
+      // TODO Auto-generated constructor stub
+   }
+
+   @Override
+   public void initialConfiguration(SolrQuery params) throws SearchException
+   {
+      /*
+       * Using eDisMax seemed like a more adventagous way of doing the query. This will allow
+       * additional solr Paramaters to be set in order to 'fine tune' the query.
+       */
+      params.set("defType", "edismax");
+   }
+
+   // TODO ulg -- can we configure some of this in a plugin?
+   @Override
+   public Class<PageSearchProxy> getSearchProxyType()
+   {
+      return PageSearchProxy.class;
+   }
+
+   @Override
+   public Class<PageTextDocument> getIndexDocumentType()
+   {
+      return PageTextDocument.class;
+   }
+
+   @Override
+   public Collection<? extends SolrIndexField<?>> getIndexedFields()
+   {
+      return Arrays.asList(ID, TEXT, NUMBER, SEQUENCE);
+   }
+
+   @Override
+   public Collection<? extends SolrIndexField<?>> getStoredFields()
+   {
+      return Arrays.asList(ID, TEXT, NUMBER, SEQUENCE);
+   }
+
+   @Override
+   public Collection<? extends SolrIndexField<?>> getMultiValuedFields()
+   {
+      return Collections.emptyList();
+   }
+
+}
