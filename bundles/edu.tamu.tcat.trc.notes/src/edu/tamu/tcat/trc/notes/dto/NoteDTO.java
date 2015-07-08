a9ad1d0bae65ae3fe2e96f3cf99951bd3194e88d
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-07-08 13:13:45 -0500

    Moved BasicNote to be inner class of DTO
    https://issues.citd.tamu.edu/browse/TRC-2
    This pattern needs to be revisited. DTOs should be simplified.

--- bundles/edu.tamu.tcat.trc.notes/src/edu/tamu/tcat/trc/notes/dto/NoteDTO.java
+++ bundles/edu.tamu.tcat.trc.notes/src/edu/tamu/tcat/trc/notes/dto/NoteDTO.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.util.UUID;
 
 import edu.tamu.tcat.trc.notes.Note;
-import edu.tamu.tcat.trc.notes.internal.dto.BasicNote;
 
 public class NoteDTO
 {
@@ -43,6 +42,54 @@ public class NoteDTO
       dto.content = orig.content;
 
       return dto;
+   }
+
+   private static class BasicNote implements Note
+   {
+      private final UUID id;
+      private final URI associatedEntity;
+      private final String authorId;
+      private final String mimeType;
+      private final String content;
+
+      public BasicNote(UUID id, URI associatedEntity, String authorId, String mimeType, String content)
+      {
+         this.id = id;
+         this.associatedEntity = associatedEntity;
+         this.authorId = authorId;
+         this.mimeType = mimeType;
+         this.content = content;
+      }
+
+      @Override
+      public UUID getId()
+      {
+         return id;
+      }
+
+      @Override
+      public URI getEntity()
+      {
+         return associatedEntity;
+      }
+
+      @Override
+      public UUID getAuthorId()
+      {
+         return UUID.fromString(authorId);
+      }
+
+      @Override
+      public String getMimeType()
+      {
+         return mimeType;
+      }
+
+      @Override
+      public String getContent()
+      {
+         return content;
+      }
    }
 
 }
