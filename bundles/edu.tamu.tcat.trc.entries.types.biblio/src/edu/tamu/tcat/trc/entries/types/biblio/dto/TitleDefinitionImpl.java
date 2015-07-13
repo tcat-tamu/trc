4795efd48546695a13f93c84202ab4b74f5ca3df
Jesse Mitchell <jessemitchell@tees.tamus.edu>
2015-07-13 08:21:59 -0500

    Bug fixes while testing.

--- bundles/edu.tamu.tcat.trc.entries.types.biblio/src/edu/tamu/tcat/trc/entries/types/biblio/dto/TitleDefinitionImpl.java
+++ bundles/edu.tamu.tcat.trc.entries.types.biblio/src/edu/tamu/tcat/trc/entries/types/biblio/dto/TitleDefinitionImpl.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -54,9 +54,7 @@ public class TitleDefinitionImpl implements TitleDefinition
    @Override
    public Set<Title> getAlternateTitles()
    {
-      return titles.stream()
-            .filter(t -> !"canonical".equalsIgnoreCase(t.getType()))
-            .collect(Collectors.toSet());
+      return new HashSet<Title>(titles);
    }
 
    @Override
