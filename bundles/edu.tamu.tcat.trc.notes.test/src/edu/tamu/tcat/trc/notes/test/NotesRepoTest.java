88409b9532c1352648f714403a4829255e408fe3
Jesse Mitchell <jessemitchell@tees.tamus.edu>
2015-07-14 12:35:02 -0500

    Updated JUnit test for Notes to match that of Articles.

--- bundles/edu.tamu.tcat.trc.notes.test/src/edu/tamu/tcat/trc/notes/test/NotesRepoTest.java
+++ bundles/edu.tamu.tcat.trc.notes.test/src/edu/tamu/tcat/trc/notes/test/NotesRepoTest.java
@@ -1,9 +1,10 @@
 package edu.tamu.tcat.trc.notes.test;
 
+
 import java.net.URI;
+import java.sql.PreparedStatement;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -18,11 +19,11 @@ import edu.tamu.tcat.db.core.DataSourceException;
 import edu.tamu.tcat.db.postgresql.exec.PostgreSqlExecutor;
 import edu.tamu.tcat.osgi.config.file.SimpleFileConfigurationProperties;
 import edu.tamu.tcat.sda.catalog.psql.provider.PsqlDataSourceProvider;
+import edu.tamu.tcat.trc.entries.repo.NoSuchCatalogRecordException;
 import edu.tamu.tcat.trc.notes.Note;
 import edu.tamu.tcat.trc.notes.dto.NoteDTO;
 import edu.tamu.tcat.trc.notes.postgres.PsqlNotesRepo;
 import edu.tamu.tcat.trc.notes.repo.EditNoteCommand;
-import edu.tamu.tcat.trc.notes.search.solr.NotesIndexManagerService;
 
 public class NotesRepoTest
 {
@@ -31,7 +32,7 @@ public class NotesRepoTest
    private SimpleFileConfigurationProperties config;
    private PsqlDataSourceProvider dsp;
    private PsqlNotesRepo repo;
-   private NotesIndexManagerService noteService;
+
 
    @BeforeClass
    public static void setUp()
@@ -63,34 +64,104 @@ public class NotesRepoTest
       repo = new PsqlNotesRepo();
       repo.setDatabaseExecutor(exec);
       repo.activate();
-
-      noteService = new NotesIndexManagerService();
-      noteService.setNotesRepo(repo);
-      noteService.setConfiguration(config);
-      noteService.activate();
    }
 
    @After
    public void tearDownTest() throws InterruptedException, ExecutionException
    {
+      String sql = "DELETE FROM notes WHERE note->>'associatedEntity' LIKE 'notes/%'";
+      Future<Void> future = exec.submit((conn) -> {
+         try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.executeUpdate();
+            return null;
+         }
+      });
 
+      future.get();
+      repo.dispose();
+      exec.close();
+      dsp.dispose();
+      config.dispose();
+   }
+
+   @Test
+   public void createNote() throws InterruptedException, ExecutionException, NoSuchCatalogRecordException
+   {
+      NoteDTO note = createNoteDTO();
+      EditNoteCommand command = repo.create();
+      command.setAll(note);
+      note.id = command.execute().get();
+
+
+      Note note2 = repo.get(note.id);
+      NoteDTO noteDTO = NoteDTO.create(note2);
+
+      Assert.assertEquals("Notes do not match", note.id, noteDTO.id);
+      Assert.assertEquals("Notes do not match", note.associatedEntity, noteDTO.associatedEntity);
+      Assert.assertEquals("Notes do not match", note.authorId, noteDTO.authorId);
+      Assert.assertEquals("Notes do not match", note.mimeType, noteDTO.mimeType);
+      Assert.assertEquals("Notes do not match", note.content, noteDTO.content);
+   }
+
+   @Test
+   public void updateNote() throws InterruptedException, ExecutionException, NoSuchCatalogRecordException
+   {
+      NoteDTO note = createNoteDTO();
+      EditNoteCommand command = repo.create();
+      command.setAll(note);
+      note.id = command.execute().get();
+
+      note.content = "<H1>The New and Everlasting Title<H1> <p>As time passes so do many articles. In this" +
+                  "particular case, this article will not be passed on. It will forever be made available" +
+                  "through this testing process. </p> " +
+                  " <p> To change the article, we need to provide some type of update to it.</p>";
+
+      EditNoteCommand updateCommand = repo.edit(note.id);
+      updateCommand.setAll(note);
+      updateCommand.execute();
+
+      Note note2 = repo.get(note.id);
+      NoteDTO noteDTO = NoteDTO.create(note2);
+
+      Assert.assertEquals("Notes do not match", note.id, noteDTO.id);
+      Assert.assertEquals("Notes do not match", note.associatedEntity, noteDTO.associatedEntity);
+      Assert.assertEquals("Notes do not match", note.authorId, noteDTO.authorId);
+      Assert.assertEquals("Notes do not match", note.mimeType, noteDTO.mimeType);
+      Assert.assertEquals("Notes do not match", note.content, noteDTO.content);
 
    }
 
    @Test
-   public void testNotesCreation() throws Exception
+   public void deleteNote() throws InterruptedException, ExecutionException
    {
-      EditNoteCommand create = repo.create();
-      NoteDTO updateDTO = new NoteDTO();
-      updateDTO.authorId = UUID.randomUUID().toString();
-      updateDTO.associatedEntity = URI.create("notes/1");
-      updateDTO.content = "The contents of the note";
-      updateDTO.mimeType = "Text";
-      create.setAll(updateDTO);
+      NoteDTO note = createNoteDTO();
+      EditNoteCommand command = repo.create();
+      command.setAll(note);
+      note.id = command.execute().get();
 
-      Future<UUID> promise = create.execute();
-      Note note = repo.get(promise.get());
+      Boolean removed = repo.remove(note.id).get();
+      Assert.assertEquals("Note was not removed", Boolean.TRUE, removed);
+      try
+      {
+         repo.get(note.id);
+         Assert.fail();
+      }
+      catch(NoSuchCatalogRecordException e)
+      {
+         Assert.assertTrue("Article has been removed", true);
+      }
 
-      Assert.assertEquals(updateDTO.authorId, note.getAuthorId());
    }
+
+   private NoteDTO createNoteDTO()
+   {
+      NoteDTO article = new NoteDTO();
+      article.associatedEntity = URI.create("notes/1");
+      article.authorId = "d25d7b89-6634-4895-89c1-7024fc3d5396";
+      article.mimeType = "Text";
+      article.content = "I'm not sure that I agree with the information that is contained within this work.";
+
+      return article;
+   }
+
 }
