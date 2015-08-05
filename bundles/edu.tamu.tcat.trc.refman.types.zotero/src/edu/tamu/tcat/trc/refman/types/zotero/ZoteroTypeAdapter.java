778524645b122d5632268a1e7de43756f2eaba55
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-08-05 10:19:53 -0500

    Removed static/instance member access inconsistencies
    https://issues.citd.tamu.edu/browse/TRC-53

--- bundles/edu.tamu.tcat.trc.refman.types.zotero/src/edu/tamu/tcat/trc/refman/types/zotero/ZoteroTypeAdapter.java
+++ bundles/edu.tamu.tcat.trc.refman.types.zotero/src/edu/tamu/tcat/trc/refman/types/zotero/ZoteroTypeAdapter.java
@@ -2,6 +2,7 @@ package edu.tamu.tcat.trc.refman.types.zotero;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,86 +14,119 @@ import edu.tamu.tcat.trc.refman.types.zotero.jaxb.TypeMap;
 import edu.tamu.tcat.trc.refman.types.zotero.jaxb.Var;
 import edu.tamu.tcat.trc.refman.types.zotero.jaxb.ZoteroMap;
 
-public class ZoteroTypeAdapter 
+/**
+ * Adapts data from the Zotero-CSL map XML (via JAXB) into the item type API defined by RefMan.
+ */
+public class ZoteroTypeAdapter
 {
-	private static ZoteroMap zMap;
-
-	public ZoteroTypeAdapter(ZoteroMap map)
+	private ZoteroTypeAdapter()
 	{
-		ZoteroTypeAdapter.zMap = map;
 	}
-	
-	public static Collection<ItemFieldType> getDefinedFields()
+
+	public static Collection<ItemFieldType> getDefinedFields(ZoteroMap zMap)
 	{
+	   // NOTE: using the CslFieldMap to retrieve all field types. This may lead to
+	   //       inconsistencies if the XML mapping file does not map all fields used in types
+	   //       to CSL values
+
+	   // FIXME: Missing fields such as dictionary title that map to CSL fields via 'base' attr
+	   // FIXME: need to get type
+
 		Collection<ItemFieldType> itemFieldTypes = new HashSet<>();
+		// FIXME -- not a java.util.map. This is an XML element that maps one Zotero field (or a family of fields)
+		//          to their coorresponding CSL field
 		Map[] mapArray = zMap.getCslFieldMap().getMap();
+		java.util.Map<String, Var> cslFields = getCSLFields(zMap);
+
 		Var[] varArray = zMap.getCslVars().getVars().getVar();
-		for(Map map : mapArray)
+		for (Map map : mapArray)
 		{
 			String id = map.getZField();
-			String label = map.getZField();
-			String description = "";
-			for(Var var : varArray)
-			{
-				String name = var.getName();
-				if(map.getCslField().equals(name))
-				{
-					description = var.getDescription();
-					continue;
-				}
-			}
-			
-			itemFieldTypes.add(new ItemFieldTypeImpl(id, label, "", "", description));
+			String label = map.getZField();     // TODO not defined here. . . labels are provided by the type defns.
+
+			Var cslField = cslFields.get(map.getCslField());
+
+			// TODO may need to convert CSL defined field types into our own internal representations
+
+
+
+			// FIXME: there is a problem with the API here.
+			ItemFieldTypeImpl field = new ItemFieldTypeImpl(id, label, cslField.getType(), "", cslField.getDescription());
+         itemFieldTypes.add(field);
 		}
+
 		return itemFieldTypes;
 	}
-	
-	public static Collection<ItemType> getDefinedTypes()
+
+	/**
+	 *
+	 * @param zMap
+	 * @return A map of defined CSL fields (a {@code var} element in the
+	 *      {@code typeMap.xml} data definition file), keyed by the CSL field name.
+	 */
+	public static java.util.Map<String, Var> getCSLFields(ZoteroMap zMap)
+	{
+	   java.util.Map<String, Var> cslFieldMap = new HashMap<>();
+	   Var[] definedCslFields = zMap.getCslVars().getVars().getVar();
+	   for (Var v : definedCslFields)
+	   {
+	      cslFieldMap.put(v.getName(), v);
+	   }
+
+	   return cslFieldMap;
+
+	}
+
+   private static String getFieldDescription(Var[] varArray, Map map)
+   {
+      String description = "";
+      for (Var var : varArray)
+      {
+      	String name = var.getName();
+      	if (map.getCslField().equals(name))
+      		return var.getDescription();
+      }
+      return description;
+   }
+
+	public static Collection<ItemType> getDefinedTypes(ZoteroMap zMap)
 	{
 		Collection<ItemType> itemTypes = new HashSet<>();
 		Var[] varArray = zMap.getCslVars().getVars().getVar();
 		TypeMap[] typeMaps = zMap.getZTypes().getTypeMap();
-		
+
 		for(TypeMap typeMap : typeMaps)
 		{
 			String typeId = typeMap.getZType();
 			String typeLabel = typeMap.getZType();
-			
+
 			List<ItemFieldType> fieldTypes = new ArrayList<>();
-			
-			if(typeMap.getField() != null)
+
+			Field[] fields = typeMap.getField();
+			if (fields == null)
+			   continue;      // some types have no defined fields.
+
+			for (Field field : fields)
 			{
-				for(Field field : typeMap.getField())
-				{
-					String id = field.getValue();
-					String label = field.getLabel();
-					String description = "";
-					for(Var var : varArray)
-					{
-						String name = var.getName();
-						if(field.getValue().equals(name))
-						{
-							description = var.getDescription();
-							continue;
-						}
-					}
-					fieldTypes.add(new ItemFieldTypeImpl(id, label, "", "", description));
-				}
+			   String id = field.getValue();
+			   String label = field.getLabel();
+			   String description = "";
+			   for(Var var : varArray)
+			   {
+			      String name = var.getName();
+			      if(field.getValue().equals(name))
+			      {
+			         description = var.getDescription();
+			         continue;
+			      }
+			   }
+			   fieldTypes.add(new ItemFieldTypeImpl(id, label, "", "", description));
 			}
+
 			itemTypes.add(new ItemTypeImpl(typeId, typeLabel, "", fieldTypes));
 		}
-		
+
 		return itemTypes;
 	}
-	
-	ItemType getType(String typeId)
-	{
-		Collection<ItemType> definedTypes = getDefinedTypes();
-		for(ItemType item : definedTypes)
-		{
-			if(item.getId().equals(typeId))
-				return item;
-		}
-		return null;
-	}
+
 }
--- bundles/edu.tamu.tcat.trc.refman.types.zotero/src/edu/tamu/tcat/trc/refman/types/zotero/ZoteroTypeProvider.java
+++ bundles/edu.tamu.tcat.trc.refman.types.zotero/src/edu/tamu/tcat/trc/refman/types/zotero/ZoteroTypeProvider.java
@@ -3,6 +3,7 @@ package edu.tamu.tcat.trc.refman.types.zotero;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.Collections;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -19,51 +20,62 @@ public class ZoteroTypeProvider implements ItemTypeProvider
 
 	public static final String ZOTERO_MXL = "edu.tamu.tcat.trc.refman.types.zotero.xml";
 	private ConfigurationProperties config;
-	private ZoteroTypeAdapter zoteroAdapter;
+   private Collection<ItemType> definedTypes;
+   private Collection<ItemFieldType> definedFields;
 
 	public void setConfiguration(ConfigurationProperties cp)
 	{
 		this.config = cp;
 	}
-	
+
 	public void activate()
 	{
 		try
 		{
+		   // TODO need to perform in separate thread
 			Path xmlPath = Paths.get(config.getPropertyValue(ZOTERO_MXL, String.class));
 			if (!xmlPath.toFile().exists())
 				throw new IllegalStateException("Unable to find the file provided:" + xmlPath.toString());
-			
+
 			JAXBContext jaxbContext = JAXBContext.newInstance( ZoteroMap.class );
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-			zoteroAdapter = new ZoteroTypeAdapter((ZoteroMap)jaxbUnmarshaller.unmarshal(xmlPath.toFile()));
-		} 
-		catch (JAXBException e) 
+			ZoteroMap zMap = (ZoteroMap)jaxbUnmarshaller.unmarshal(xmlPath.toFile());
+
+			definedFields = ZoteroTypeAdapter.getDefinedFields(zMap);
+			definedTypes = ZoteroTypeAdapter.getDefinedTypes(zMap);
+		}
+		catch (JAXBException e)
 		{
 			throw new IllegalStateException("An error occurred while attempting to unmarshall the xml file to the ZoteroMap.class.\n" + e);
 		}
 	}
-	
+
 	public void deactivate()
 	{
 	}
 
 	@Override
-	public Collection<ItemFieldType> listDefinedFields() 
+	public Collection<ItemFieldType> listDefinedFields()
 	{
-		return zoteroAdapter.getDefinedFields();
+		return Collections.unmodifiableCollection(definedFields);
 	}
 
 	@Override
-	public Collection<ItemType> listDefinedTypes() 
+	public Collection<ItemType> listDefinedTypes()
 	{
-		return zoteroAdapter.getDefinedTypes();
+	   return Collections.unmodifiableCollection(definedTypes);
 	}
 
 	@Override
-	public ItemType getItemType(String typeId) throws IllegalArgumentException 
+	public ItemType getItemType(String typeId) throws IllegalArgumentException
 	{
-		return zoteroAdapter.getType(typeId);
+      for (ItemType item : definedTypes)
+      {
+         if(item.getId().equals(typeId))
+            return item;
+      }
+
+      return null;
 	}
 
 
