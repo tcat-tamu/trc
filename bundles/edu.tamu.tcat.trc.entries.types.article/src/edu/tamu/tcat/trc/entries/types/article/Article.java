bf39f44477b5c499b09ac65357d694b8bd20f6b7
Neal Audenaert <neala@tamu.edu>
2015-09-23 13:59:56 -0500

    Added javadoc and revised comments
    https://issues.citd.tamu.edu/browse/TRC-38

--- bundles/edu.tamu.tcat.trc.entries.types.article/src/edu/tamu/tcat/trc/entries/types/article/Article.java
+++ bundles/edu.tamu.tcat.trc.entries.types.article/src/edu/tamu/tcat/trc/entries/types/article/Article.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,8 +18,25 @@ package edu.tamu.tcat.trc.entries.types.article;
 import java.net.URI;
 import java.util.UUID;
 
+/**
+ *  A long-form article that provides discursive treatment of a topic within a thematic
+ *  research collection. The long-form article is a mainstay of scholarly communication, that
+ *  allows for an extended narrative treatment of a particular subject.
+ *
+ *  <p>
+ *  The content of an article will typically be provided as lightly marked semantic HTML.
+ *  Alternative representations include XML, TEI, Markdown, Text.
+ *
+ *  <p>
+ *  This API is currently provisional. It will be extended and revised significantly prior to
+ *  its 1.0 release.
+ */
 public interface Article
 {
+   // TODO support multiple authors, possibly with different roles in contributing to the work
+   // TODO consider support for multiple content formats (microsummary, overview, article). May implement separately
+   // TODO consider support for translations
+   // TODO support attachments and/or alternate representations (PDF)
 
    /**
     * @return A unique identifier for this article.
@@ -27,14 +44,16 @@ public interface Article
    UUID getId();
 
    /**
-    * @return String representation of the Title of the Article.
+    * @return String representation of the title of the article.
     */
    String getTitle();
 
    /**
-    * @return URI of the entity to which they are attached. Note that this may be an element
-    *    within a catalog entry (e.g., attached to an author reference) once the entry type
-    *    itself provides well-defined URIs for those internal components.
+    * This is API is provisional. It is under review to determine if this use case would be
+    * better met through a different mechanism.
+    *
+    * @return URI of an entity to which this article is associated. For example, if this article
+    *    is a book review, this would be the URI of the corresponding bibliographic entry.
     */
    URI getEntity();
 
--- bundles/edu.tamu.tcat.trc.entries.types.article/src/edu/tamu/tcat/trc/entries/types/article/dto/ArticleDTO.java
+++ bundles/edu.tamu.tcat.trc.entries.types.article/src/edu/tamu/tcat/trc/entries/types/article/dto/ArticleDTO.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,15 +29,15 @@ public class ArticleDTO
    public String mimeType;
    public String content;
 
-
    public static ArticleDTO create(Article article)
    {
       ArticleDTO dto = new ArticleDTO();
+      UUID author = article.getAuthorId();
 
       dto.id = article.getId();
       dto.title = article.getTitle();
       dto.associatedEntity = article.getEntity();
-      dto.authorId = article.getAuthorId() != null ? article.getAuthorId().toString() : "";
+      dto.authorId = author != null ? author.toString() : "";
       dto.mimeType = article.getMimeType();
       dto.content = article.getContent();
 
