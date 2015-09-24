5b44ee869ab85e3e96d918ce655bd9c54dd6a745
Neal Audenaert <neala@tamu.edu>
2015-09-24 16:19:17 -0500

    warnings cleanup

--- bundles/edu.tamu.tcat.trc.entries.types.article.postgres/src/edu/tamu/tcat/trc/entries/types/article/postgres/PsqlArticleRepo.java
+++ bundles/edu.tamu.tcat.trc.entries.types.article.postgres/src/edu/tamu/tcat/trc/entries/types/article/postgres/PsqlArticleRepo.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,7 +28,6 @@ import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -66,11 +65,6 @@ public class PsqlArticleRepo implements ArticleRepository
          "SELECT article "
                +  "FROM articles "
                + "WHERE article_id = ? AND active = true";
-
-   private static final String SQL_GET_ALL_BY_ID =
-         "SELECT article "
-        +  "FROM articles "
-        + "WHERE article_id = ?";
 
    private static String CREATE_SQL =
          "INSERT INTO articles (article, article_id) VALUES(?, ?)";
