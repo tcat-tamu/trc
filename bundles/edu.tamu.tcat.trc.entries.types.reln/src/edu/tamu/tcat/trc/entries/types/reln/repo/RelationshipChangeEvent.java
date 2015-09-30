7c0d8e9cdda0e347052912ef93b1d0988bd1ca89
Neal Audenaert <neala@tamu.edu>
2015-09-29 20:26:32 -0500

    Deprecated #getRelationship and provided default impl

--- bundles/edu.tamu.tcat.trc.entries.types.reln/src/edu/tamu/tcat/trc/entries/types/reln/repo/RelationshipChangeEvent.java
+++ bundles/edu.tamu.tcat.trc.entries.types.reln/src/edu/tamu/tcat/trc/entries/types/reln/repo/RelationshipChangeEvent.java
@@ -16,9 +16,32 @@
 package edu.tamu.tcat.trc.entries.types.reln.repo;
 
 import edu.tamu.tcat.trc.entries.notification.UpdateEvent;
+import edu.tamu.tcat.trc.entries.repo.CatalogRepoException;
+import edu.tamu.tcat.trc.entries.types.reln.Relationship;
 
 public interface RelationshipChangeEvent extends UpdateEvent
 {
 
+   /**
+    * Retrieves the element that was changed. Implementations should attempt to return
+    * a copy of the element in the state it was immediately after the change occurred.
+    *
+    * @return the element that was changed.
+    * @throws CatalogRepoException If the element cannot be retrieved. This is expected
+    *    in the case of {@link UpdateAction#DELETED} events. In other cases, this is not
+    *    expected but due to an internal error.
+    * @deprecated This API is problematic.
+    *    For deletions, this is not available or may be the last state before removal, though
+    *    it may be expensive to retain and pass around if nothing actually needs it.
+    *    For additions, the entire element should be captured to prevent a subsequent lookup
+    *    by a listener from getting a newer state than what it was created as.
+    *    For mutations, the state of the element just after mutation should be captured though
+    *    it would be less data to only store the properties that changed. If one of these
+    *    "properties" carries the entire object, the creation case may be reduced to it.
+    */
+   @Deprecated
+   default Relationship getRelationship() throws CatalogRepoException {
+      return null;
+   }
 
 }
