8063aebc203282541c3928a19af70ea41f3d8deb
Matthew Barry <mbarry@tamu.edu>
2016-01-29 13:28:06 -0600

    remove default text from server-side: leave null and let client decide default text to display

--- bundles/edu.tamu.tcat.trc.entries.types.bio.rest/src/edu/tamu/tcat/trc/entries/types/bio/rest/v1/SearchAdapter.java
+++ bundles/edu.tamu.tcat.trc.entries.types.bio.rest/src/edu/tamu/tcat/trc/entries/types/bio/rest/v1/SearchAdapter.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,8 +27,6 @@ import edu.tamu.tcat.trc.entries.types.bio.search.BioSearchProxy;
  */
 public class SearchAdapter
 {
-   public static final String DEFAULT_SUMMARY = "No summary text has been provided for this person yet.";
-
    public static List<RestApiV1.PersonSearchResult> toDTO(List<BioSearchProxy> origList)
    {
       if (origList == null)
@@ -40,7 +38,7 @@ public class SearchAdapter
          dto.id = orig.id;
          dto.displayName = toDTO(orig.displayName);
          dto.formattedName = orig.formattedName;
-         dto.summary = orig.summaryExcerpt == null ? DEFAULT_SUMMARY : orig.summaryExcerpt;
+         dto.summary = orig.summaryExcerpt;
 
          dtoList.add(dto);
       }
