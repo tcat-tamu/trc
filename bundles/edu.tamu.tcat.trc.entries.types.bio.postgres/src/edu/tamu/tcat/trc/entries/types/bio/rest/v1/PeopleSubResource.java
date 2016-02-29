a27cca8ebdadbec26ad4d0d76e672cc2e3bcca99
Neal Audenaert <neala@tamu.edu>
2016-02-29 17:32:00 -0600

    Refactored REST API implementation; improved error handling

---
+++ bundles/edu.tamu.tcat.trc.entries.types.bio.postgres/src/edu/tamu/tcat/trc/entries/types/bio/rest/v1/PeopleSubResource.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2015 Texas A&M Engineering Experiment Station
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.tamu.tcat.trc.entries.types.bio.rest.v1;
+
+import java.net.URLEncoder;
+import java.text.MessageFormat;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import edu.tamu.tcat.trc.entries.types.bio.repo.EditPersonCommand;
+import edu.tamu.tcat.trc.entries.types.bio.repo.PeopleRepository;
+import edu.tamu.tcat.trc.entries.types.bio.rest.v1.internal.RepoAdapter;
+import edu.tamu.tcat.trc.entries.types.bio.rest.v1.internal.SearchAdapter;
+import edu.tamu.tcat.trc.entries.types.bio.search.PeopleQueryCommand;
+import edu.tamu.tcat.trc.entries.types.bio.search.PeopleSearchService;
+import edu.tamu.tcat.trc.entries.types.bio.search.PersonSearchResult;
+import edu.tamu.tcat.trc.search.SearchException;
+
+public class PeopleSubResource
+{
+   // records internal errors accessing the REST
+   static final Logger errorLogger = Logger.getLogger(PeopleSubResource.class.getName());
+
+   private PeopleRepository repo;
+   private PeopleSearchService peopleSearchService;
+
+   public PeopleSubResource(PeopleRepository repo, PeopleSearchService search)
+   {
+      this.repo = repo;
+      peopleSearchService = search;
+   }
+
+   @GET
+   @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+   public RestApiV1.PersonSearchResultSet
+   searchPeople(@QueryParam(value="q") String q,
+                @QueryParam(value = "off") @DefaultValue("0")   int offset,
+                @QueryParam(value = "max") @DefaultValue("100") int numResults)
+   {
+      try
+      {
+         PeopleQueryCommand cmd = peopleSearchService.createQueryCommand();
+         if (q != null)
+            cmd.query(q);
+         cmd.setOffset(offset);
+         cmd.setMaxResults(numResults);
+         PersonSearchResult results = cmd.execute();
+
+         RestApiV1.PersonSearchResultSet rs = new RestApiV1.PersonSearchResultSet();
+         rs.items = SearchAdapter.toDTO(results.get());
+
+         buildQueryLinks(rs, q, offset, numResults);
+
+         return rs;
+      }
+      catch (SearchException e)
+      {
+         errorLogger.log(Level.SEVERE, "Error", e);
+         throw new InternalServerErrorException("Failed to execute search", e);
+      }
+   }
+
+   private void buildQueryLinks(RestApiV1.PersonSearchResultSet rs, String q, int offset, int numResults)
+   {
+      //TODO: does next link depend on the number of results returned (i.e. whether < numResults), or do we assume there are infinite results?
+      try
+      {
+         String linktemplate = "off={0}&max={1}&q={2}";
+         String query = q != null ? URLEncoder.encode(q, "UTF-8") : "";
+
+         rs.qs = MessageFormat.format(linktemplate, offset, numResults, query);
+         rs.qsNext = MessageFormat.format(linktemplate, (offset + numResults), numResults, query);
+         if (offset >= numResults)
+            rs.qsPrev = MessageFormat.format(linktemplate, (offset - numResults), numResults, query);
+         // first page got off; reset to zero offset
+         else if (offset > 0 && offset < numResults)
+            rs.qsPrev = MessageFormat.format(linktemplate, 0, numResults, query);
+      }
+      catch (Exception e)
+      {
+         throw new InternalServerErrorException("Failed building querystring", e);
+      }
+   }
+
+   @POST
+   @Consumes(MediaType.APPLICATION_JSON)
+   @Produces(MediaType.APPLICATION_JSON)
+   public RestApiV1.PersonId createPerson(RestApiV1.Person person)
+   {
+      try
+      {
+         EditPersonCommand createCommand = repo.create();
+         createCommand.setAll(RepoAdapter.toRepo(person));
+
+         RestApiV1.PersonId personId = new RestApiV1.PersonId();
+         personId.id = createCommand.execute().get();
+         return personId;
+      }
+      catch (InterruptedException | ExecutionException e)
+      {
+         throw new InternalServerErrorException();
+      }
+   }
+
+   @Path("{personId}")
+   public PersonResource getPerson(@PathParam(value="personId") String personId)
+   {
+      return new PersonResource(repo, personId);
+   }
+}
