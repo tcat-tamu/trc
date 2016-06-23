63cff553213a5c1651e8613d50f9265b41bc7f8f
Neal Audenaert <neala@tamu.edu>
2016-06-23 17:17:04 -0500

    Remove legacy ReferenceManagement impl and unit tests
    https://issues.citd.tamu.edu/browse/TRC-231

--- bundles/edu.tamu.tcat.trc.refman.postgres/src/edu/tamu/tcat/trc/refman/postgres/RefCollectionMeta.java
+++
@@ -1,9 +0,0 @@
-package edu.tamu.tcat.trc.refman.postgres;
-
-public class RefCollectionMeta
-{
-   public String id;
-   public String name;
-   public String description;
-   public String providerId;
-}
