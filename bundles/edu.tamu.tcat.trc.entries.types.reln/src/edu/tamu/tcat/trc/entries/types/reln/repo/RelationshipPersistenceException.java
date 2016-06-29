a2e8d0166b5acdc4a08287060a8a33425f99b939
Neal Audenaert <neala@tamu.edu>
2016-06-29 15:34:07 -0500

    Remove ExecutionFailedException

--- bundles/edu.tamu.tcat.trc.entries.types.reln/src/edu/tamu/tcat/trc/entries/types/reln/repo/RelationshipPersistenceException.java
+++ bundles/edu.tamu.tcat.trc.entries.types.reln/src/edu/tamu/tcat/trc/entries/types/reln/repo/RelationshipPersistenceException.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,12 +15,10 @@
  */
 package edu.tamu.tcat.trc.entries.types.reln.repo;
 
-import edu.tamu.tcat.trc.entries.repo.CatalogRepoException;
-
 /**
  * Indicates that an attempt to access the underlying persistence layer failed for some reason.
  */
-public class RelationshipPersistenceException extends CatalogRepoException
+public class RelationshipPersistenceException extends Exception
 {
    public RelationshipPersistenceException()
    {
