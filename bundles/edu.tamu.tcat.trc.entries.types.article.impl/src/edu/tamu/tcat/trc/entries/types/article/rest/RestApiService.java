887c6012b0d6d49ffa1e5498fa04da77d1bf7206
Neal Audenaert <neala@tamu.edu>
2016-07-05 11:55:45 -0500

    Configure main point of entry for article REST API

--- bundles/edu.tamu.tcat.trc.entries.types.article.impl/src/edu/tamu/tcat/trc/entries/types/article/rest/RestApiService.java
+++ bundles/edu.tamu.tcat.trc.entries.types.article.impl/src/edu/tamu/tcat/trc/entries/types/article/rest/RestApiService.java
@@ -1,24 +1,38 @@
 package edu.tamu.tcat.trc.entries.types.article.rest;
 
+import java.net.URI;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.ws.rs.Path;
 
 import edu.tamu.tcat.osgi.config.ConfigurationProperties;
-import edu.tamu.tcat.trc.entries.types.article.repo.ArticleRepository;
+import edu.tamu.tcat.trc.entries.types.article.docrepo.ArticleRepoService;
+import edu.tamu.tcat.trc.entries.types.article.rest.v1.ArticlesCollectionResource;
 
 /**
  * The primary REST end point for articles. This is intended to be
  * registered as an OSGi declarative service and to return the specific
  * elements of the REST API as sub-resources.
  */
-@Path("/articles")
+@Path("/")
 public class RestApiService
 {
-   private ArticleRepository repo;
-   private ConfigurationProperties config;
+   private static final String CONFIG_API_ENDPOINT = "edu.tamu.tcat.trc.rest.endpoint";
 
-   public void bindArticleRepository(ArticleRepository repo)
+   private final static Logger logger = Logger.getLogger(RestApiService.class.getName());
+
+   private static final String ARTICLES_PATH = "articles";
+//   private static final String AUTHORS_PATH = "article_authors";
+
+   private ArticleRepoService repoSvc;
+   private ConfigurationProperties config;
+   private URI endpoint;
+
+   public void bindArticleRepository(ArticleRepoService repoSvc)
    {
-      this.repo = repo;
+      this.repoSvc = repoSvc;
    }
 
    public void bindConfig(ConfigurationProperties config)
@@ -28,11 +42,32 @@ public class RestApiService
 
    public void activate()
    {
+      try
+      {
+         Objects.requireNonNull(repoSvc, "Article repository service is not configured.");
 
+         String apiEndpoint = null;
+         if (config != null)
+            apiEndpoint = config.getPropertyValue(CONFIG_API_ENDPOINT, String.class);
+         endpoint = apiEndpoint != null ? URI.create(apiEndpoint) : URI.create("/");
+         if (apiEndpoint == null)
+            logger.warning("No API endpoint for TRC is configured. Links returned via the REST API will not function correctly.");
+      }
+      catch (Exception ex)
+      {
+         logger.log(Level.SEVERE, "Failed to activate Article RestApiService", ex);
+         throw ex;
+      }
    }
 
    public void deactivate()
    {
 
+   }
+
+   @Path(ARTICLES_PATH)
+   public ArticlesCollectionResource getArticles()
+   {
+      return new ArticlesCollectionResource(repoSvc, endpoint.resolve(ARTICLES_PATH));
    }
 }
