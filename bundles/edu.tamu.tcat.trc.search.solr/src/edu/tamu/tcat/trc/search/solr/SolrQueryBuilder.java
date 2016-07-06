aceb138b9b8468a813d2b955d364a9437b881086
Neal Audenaert <neala@tamu.edu>
2016-07-06 14:04:28 -0500

    clean up comments

--- bundles/edu.tamu.tcat.trc.search.solr/src/edu/tamu/tcat/trc/search/solr/SolrQueryBuilder.java
+++ bundles/edu.tamu.tcat.trc.search.solr/src/edu/tamu/tcat/trc/search/solr/SolrQueryBuilder.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,7 +18,6 @@ package edu.tamu.tcat.trc.search.solr;
 import java.util.Collection;
 import java.util.Collections;
 
-import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.common.params.SolrParams;
 
 import edu.tamu.tcat.trc.search.SearchException;
@@ -31,8 +30,7 @@ import edu.tamu.tcat.trc.search.SearchException;
 public interface SolrQueryBuilder
 {
    /**
-    * Get the result of this builder as a {@link SolrParams}
-    * to be executed using {@link SolrServer#query(SolrParams)}
+    * Get the result of this builder as a {@link SolrParams}.
     */
    SolrParams get() throws SearchException;
 
