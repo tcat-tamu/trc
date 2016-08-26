cc87665711d66a87191df452b317f40e1c1df494
Neal Audenaert <neala@tamu.edu>
2016-08-26 11:02:53 -0500

    Add #toString on EntryReference

--- bundles/edu.tamu.tcat.trc/src/edu/tamu/tcat/trc/entries/core/resolver/EntryReference.java
+++ bundles/edu.tamu.tcat.trc/src/edu/tamu/tcat/trc/entries/core/resolver/EntryReference.java
@@ -1,5 +1,7 @@
 package edu.tamu.tcat.trc.entries.core.resolver;
 
+import static java.text.MessageFormat.format;
+
 /**
  * A reference to an entry within a TRC repository.
  */
@@ -7,10 +9,19 @@ public class EntryReference
 {
 
    // TODO this is really and id, not a reference.
+   // TODO what about sub-parts, eg. a Volume of a work?
 
    /** Defines the type of entry this reference pertains to. */
    public String type;
 
    /** The unique identifier for a particular entry. */
    public String id;
+
+   // TODO support equals, hashcode and to-string
+
+   @Override
+   public String toString()
+   {
+      return format("Entry Reference: \n\ttype={0}\n\tid={1}", type, id);
+   }
 }
