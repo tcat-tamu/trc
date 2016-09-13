608d10fa0dbca7579f260ca25bda2e25c0381311
Neal Audenaert <neala@tamu.edu>
2016-09-13 16:36:59 -0500

    update implementation based on RepoDelegate

---
+++ bundles/edu.tamu.tcat.trc.entries.types.biblio.impl/src/edu/tamu/tcat/trc/entries/types/biblio/postgres/WorkRepoImpl.java
@@ -0,0 +1,97 @@
+package edu.tamu.tcat.trc.entries.types.biblio.postgres;
+
+import static java.text.MessageFormat.format;
+
+import java.util.Iterator;
+import java.util.concurrent.CompletableFuture;
+
+import edu.tamu.tcat.account.Account;
+import edu.tamu.tcat.trc.entries.core.repo.BasicRepoDelegate;
+import edu.tamu.tcat.trc.entries.core.repo.EntryRepository;
+import edu.tamu.tcat.trc.entries.types.biblio.BibliographicEntry;
+import edu.tamu.tcat.trc.entries.types.biblio.Edition;
+import edu.tamu.tcat.trc.entries.types.biblio.Volume;
+import edu.tamu.tcat.trc.entries.types.biblio.dto.WorkDTO;
+import edu.tamu.tcat.trc.entries.types.biblio.repo.BibliographicEntryRepository;
+import edu.tamu.tcat.trc.entries.types.biblio.repo.EditBibliographicEntryCommand;
+
+public class WorkRepoImpl implements BibliographicEntryRepository
+{
+   private final Account account;
+   private final BasicRepoDelegate<BibliographicEntry, WorkDTO, EditBibliographicEntryCommand> delegate;
+
+   WorkRepoImpl(BasicRepoDelegate<BibliographicEntry, WorkDTO, EditBibliographicEntryCommand> delegate, Account account)
+   {
+      this.delegate = delegate;
+      this.account = account;
+   }
+
+   @Override
+   public Iterator<BibliographicEntry> getAllWorks()
+   {
+      return delegate.listAll();
+   }
+
+   @Override
+   public BibliographicEntry get(String id)
+   {
+      return delegate.get(account, id);
+   }
+
+   @Override
+   public EditBibliographicEntryCommand create()
+   {
+      return delegate.create(account);
+   }
+
+   @Override
+   public EditBibliographicEntryCommand create(String id)
+   {
+      return delegate.create(account, id);
+   }
+
+   @Override
+   public EditBibliographicEntryCommand edit(String id)
+   {
+      return delegate.create(account, id);
+
+   }
+
+   @Override
+   public CompletableFuture<Boolean> remove(String id)
+   {
+      return delegate.remove(account, id);
+   }
+
+   @Override
+   public Edition getEdition(String workId, String editionId)
+   {
+      String msg = "Unable to find edition with id [{0}] on work [{1}].";
+
+      BibliographicEntry work = get(workId);
+      Edition edition = work.getEdition(editionId);
+      if (edition == null)
+         throw new IllegalArgumentException(format(msg, editionId, workId));
+
+      return edition;
+   }
+
+   @Override
+   public Volume getVolume(String workId, String editionId, String volumeId)
+   {
+      Edition edition = getEdition(workId, editionId);
+      Volume volume = edition.getVolume(volumeId);
+      if (volume == null)
+      {
+         throw new IllegalArgumentException("Unable to find volume with id {" + volumeId + "} on edition {" + editionId + "} on work {" + workId + "}.");
+      }
+      return volume;
+   }
+
+   @Override
+   public EntryRepository.ObserverRegistration onUpdate(EntryRepository.UpdateObserver<BibliographicEntry> observer)
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+}
--- bundles/edu.tamu.tcat.trc.entries.types.biblio.impl/src/edu/tamu/tcat/trc/entries/types/biblio/postgres/WorkRepositoryService.java
+++ bundles/edu.tamu.tcat.trc.entries.types.biblio.impl/src/edu/tamu/tcat/trc/entries/types/biblio/postgres/WorkRepositoryService.java
@@ -1,48 +1,27 @@
 package edu.tamu.tcat.trc.entries.types.biblio.postgres;
 
-import static java.text.MessageFormat.format;
-
-import java.net.URI;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import org.apache.solr.common.SolrInputDocument;
 
 import edu.tamu.tcat.account.Account;
 import edu.tamu.tcat.osgi.config.ConfigurationProperties;
 import edu.tamu.tcat.trc.entries.core.InvalidReferenceException;
-import edu.tamu.tcat.trc.entries.core.repo.EntryRepository;
+import edu.tamu.tcat.trc.entries.core.repo.BasicRepoDelegate;
 import edu.tamu.tcat.trc.entries.core.repo.RepositoryContext;
 import edu.tamu.tcat.trc.entries.core.resolver.EntryReference;
 import edu.tamu.tcat.trc.entries.core.resolver.EntryResolverBase;
 import edu.tamu.tcat.trc.entries.types.biblio.BibliographicEntry;
-import edu.tamu.tcat.trc.entries.types.biblio.Edition;
-import edu.tamu.tcat.trc.entries.types.biblio.Volume;
 import edu.tamu.tcat.trc.entries.types.biblio.dto.WorkDTO;
 import edu.tamu.tcat.trc.entries.types.biblio.repo.BibliographicEntryRepository;
 import edu.tamu.tcat.trc.entries.types.biblio.repo.EditBibliographicEntryCommand;
-import edu.tamu.tcat.trc.entries.types.biblio.search.solr.IndexAdapter;
+import edu.tamu.tcat.trc.repo.DocRepoBuilder;
 import edu.tamu.tcat.trc.repo.DocumentRepository;
 import edu.tamu.tcat.trc.repo.IdFactory;
-import edu.tamu.tcat.trc.repo.RepositoryException;
-import edu.tamu.tcat.trc.search.SearchException;
-import edu.tamu.tcat.trc.search.solr.SolrSearchAdapter;
-import edu.tamu.tcat.trc.search.solr.impl.BasicIndexService;
 
 public class WorkRepositoryService
 {
    private static final Logger logger = Logger.getLogger(WorkRepositoryService.class.getName());
-
-   // TODO this needs to be refactored.
-   /** Configuration property key that defines the URI for the Solr server. */
-   public static final String SOLR_API_ENDPOINT = "solr.api.endpoint";
-
-   /** Configuration property key that defines Solr core to be used for relationships. */
-   public static final String SOLR_CORE = "trc.entries.biblio.solr-core";
-
 
    public static final String ID_CONTEXT_WORKS = "works";
    public static final String ID_CONTEXT_EDITIONS = "editions";
@@ -50,7 +29,6 @@ public class WorkRepositoryService
    public static final String ID_CONTEXT_COPIES = "copies";
 
    private static final String TABLE_NAME = "works";
-   private static final String SCHEMA_ID = "trcWork";
    private static final String SCHEMA_DATA_FIELD = "work";
 
    private DocumentRepository<BibliographicEntry, WorkDTO, EditBibliographicEntryCommand> repoBackend;
@@ -59,7 +37,7 @@ public class WorkRepositoryService
    private ConfigurationProperties config;
    private RepositoryContext context;
 
-   // TODO handle DB schema
+   private BasicRepoDelegate<BibliographicEntry, WorkDTO, EditBibliographicEntryCommand> delegate;
 
    public void setRepoContext(RepositoryContext context)
    {
@@ -76,80 +54,49 @@ public class WorkRepositoryService
       // TODO fix DS stitching
       try
       {
-         logger.info("Activating bibliographic entry repository service.");
+         logger.info("Activating bibliographic entry repository service. . . ");
          workIds = context.getIdFactory(ID_CONTEXT_WORKS);
          config = context.getConfig();
 
-         context.registerRepository(BibliographicEntryRepository.class, account -> new WorkRepoImpl(account));
+         initDocRepo();
+         initDelegate();
+
+         context.registerRepository(BibliographicEntryRepository.class, account -> new WorkRepoImpl(delegate, account));
          context.registerResolver(new BibliographicEntryResolver());
-
-         repoBackend = context.buildDocumentRepo(TABLE_NAME,
-               new EditWorkCommandFactory(context::getIdFactory),
-               ModelAdapter::adapt,
-               WorkDTO.class);
-
-         // TODO need to wire this into core?
-         BasicIndexService<WorkDTO> indexSvc = new BasicIndexService<>(
-               config.getPropertyValue(SOLR_API_ENDPOINT, URI.class),
-               config.getPropertyValue(SOLR_CORE, String.class),
-               WorkRepositoryService::adapt,
-               entry -> entry.id);
-         repoBackend.afterUpdate(ctx -> SolrSearchAdapter.index(indexSvc, ctx));
 
          logger.fine("Activating bibliographic entry repository service.");
       }
       catch (Exception ex)
       {
-         logger.log(Level.SEVERE, "Failed to activating bibliographic entry repository service.", ex);
-
+         logger.log(Level.SEVERE, "Failed to activate bibliographic entry repository service.", ex);
+         throw ex;
       }
    }
 
-   private static SolrInputDocument adapt(WorkDTO entry)
+   private void initDelegate()
    {
-      try
-      {
-         return IndexAdapter.createWork(entry);
-      }
-      catch (SearchException ex)
-      {
-         throw new IllegalStateException(format("Failed to adapt bibliographic entry [{0}]", entry.id), ex);
-      }
+      BasicRepoDelegate.Builder<BibliographicEntry, WorkDTO, EditBibliographicEntryCommand> delegateBuilder =
+            new BasicRepoDelegate.Builder<>();
+
+      delegateBuilder.setEntryName("bibliographic");
+      delegateBuilder.setIdFactory(workIds);
+      delegateBuilder.setEntryResolvers(context.getResolverRegistry());
+      delegateBuilder.setAdapter(ModelAdapter::adapt);
+      delegateBuilder.setDocumentRepo(repoBackend);
+
+      delegate = delegateBuilder.build();
    }
 
-   public EditBibliographicEntryCommand createWork(Account account, String id)
+   private void initDocRepo()
    {
-      return repoBackend.create(account, id);
+      DocRepoBuilder<BibliographicEntry, WorkDTO, EditBibliographicEntryCommand> builder = context.getDocRepoBuilder();
+      repoBackend = builder.setTableName(TABLE_NAME)
+             .setDataColumn(SCHEMA_DATA_FIELD)
+             .setEditCommandFactory(new EditWorkCommandFactory(context::getIdFactory))
+             .setDataAdapter(ModelAdapter::adapt)
+             .setStorageType(WorkDTO.class)
+             .build();
    }
-
-   public EditBibliographicEntryCommand editWork(Account account, String workId)
-   {
-      try
-      {
-         return repoBackend.edit(account, workId);
-      }
-      catch (RepositoryException e)
-      {
-         throw new IllegalArgumentException("Unable to find work with id {" + workId + "}.", e);
-      }
-   }
-
-   public CompletableFuture<Boolean> remove(Account account, String workId)
-   {
-      CompletableFuture<Boolean> result = new CompletableFuture<>();
-      try {
-         result = repoBackend.delete(account, workId);
-      }
-      catch (Exception e)
-      {
-         String message = "Encountered an unexpected error while trying to delete work with id [{0}].";
-         result.completeExceptionally(new IllegalStateException(format(message, workId), e));
-         return result;
-      }
-
-      return result;
-   }
-
 
    /**
     * Lifecycle management method (usually called by framework service layer)
@@ -157,100 +104,7 @@ public class WorkRepositoryService
     */
    public void dispose()
    {
-   }
-
-   private class WorkRepoImpl implements BibliographicEntryRepository
-   {
-      private final Account account;
-
-      WorkRepoImpl(Account account)
-      {
-         this.account = account;
-      }
-
-      @Override
-      public Iterator<BibliographicEntry> getAllWorks()
-      {
-         try
-         {
-            return repoBackend.listAll();
-         }
-         catch (RepositoryException e)
-         {
-            throw new IllegalStateException("Unable to list all works", e);
-         }
-      }
-
-      @Override
-      public BibliographicEntry get(String workId)
-      {
-         try
-         {
-            return repoBackend.get(workId);
-         }
-         catch (RepositoryException e)
-         {
-            String message = "Unable to find work with id [{0}].";
-            throw new IllegalArgumentException(format(message, workId), e);
-         }
-      }
-
-      @Override
-      public EditBibliographicEntryCommand create()
-      {
-         return create(workIds.get());
-      }
-
-      @Override
-      public EditBibliographicEntryCommand create(String id)
-      {
-         return WorkRepositoryService.this.createWork(account, id);
-      }
-
-      @Override
-      public EditBibliographicEntryCommand edit(String workId)
-      {
-         return WorkRepositoryService.this.editWork(account, workId);
-
-      }
-
-      @Override
-      public CompletableFuture<Boolean> remove(String workId)
-      {
-         return WorkRepositoryService.this.remove(account, workId);
-      }
-
-      @Override
-      public Edition getEdition(String workId, String editionId)
-      {
-         String msg = "Unable to find edition with id [{0}] on work [{1}].";
-
-         BibliographicEntry work = get(workId);
-         Edition edition = work.getEdition(editionId);
-         if (edition == null)
-            throw new IllegalArgumentException(format(msg, editionId, workId));
-
-         return edition;
-      }
-
-      @Override
-      public Volume getVolume(String workId, String editionId, String volumeId)
-      {
-         Edition edition = getEdition(workId, editionId);
-         Volume volume = edition.getVolume(volumeId);
-         if (volume == null)
-         {
-            throw new IllegalArgumentException("Unable to find volume with id {" + volumeId + "} on edition {" + editionId + "} on work {" + workId + "}.");
-         }
-         return volume;
-      }
-
-      @Override
-      public EntryRepository.ObserverRegistration onUpdate(EntryRepository.UpdateObserver<BibliographicEntry> observer)
-      {
-         // TODO Auto-generated method stub
-         return null;
-      }
+      repoBackend.dispose();
    }
 
    private class BibliographicEntryResolver extends EntryResolverBase<BibliographicEntry>
@@ -258,7 +112,10 @@ public class WorkRepositoryService
 
       public BibliographicEntryResolver()
       {
-         super(BibliographicEntry.class, config, BibliographicEntryRepository.ENTRY_URI_BASE, BibliographicEntryRepository.ENTRY_TYPE_ID);
+         super(BibliographicEntry.class,
+               config,
+               BibliographicEntryRepository.ENTRY_URI_BASE,
+               BibliographicEntryRepository.ENTRY_TYPE_ID);
       }
 
       @Override
@@ -267,7 +124,7 @@ public class WorkRepositoryService
          if (!accepts(reference))
             throw new InvalidReferenceException(reference, "Unsupported reference type.");
 
-         WorkRepoImpl repo = new WorkRepoImpl(account);
+         WorkRepoImpl repo = new WorkRepoImpl(delegate, account);
          return repo.get(reference.id);
       }
 
