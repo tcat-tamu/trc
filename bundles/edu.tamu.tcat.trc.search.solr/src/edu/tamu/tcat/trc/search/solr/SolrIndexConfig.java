2dd34d3d919e11179d3a4e4dfc4c270d840f7e30
Neal Audenaert <neala@tamu.edu>
2016-09-15 13:02:46 -0500

    Deprecated unused getIndexDocumentType method

--- bundles/edu.tamu.tcat.trc.search.solr/src/edu/tamu/tcat/trc/search/solr/SolrIndexConfig.java
+++ bundles/edu.tamu.tcat.trc.search.solr/src/edu/tamu/tcat/trc/search/solr/SolrIndexConfig.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -74,7 +74,10 @@ public interface SolrIndexConfig
     */
    Class<?> getSearchProxyType();
 
-   Class<?> getIndexDocumentType();
+   @Deprecated
+   default Class<?> getIndexDocumentType() {
+      throw new UnsupportedOperationException();
+   }
 
    /*
     * These methods are here in the config and not a part of every field to allow more
