2b60ce1919e65414045f7fe1eb3e12123f98a646
Neal Audenaert <neala@tamu.edu>
2016-09-15 18:07:51 -0500

    Make search exception unchecked

--- bundles/edu.tamu.tcat.trc/src/edu/tamu/tcat/trc/search/SearchException.java
+++ bundles/edu.tamu.tcat.trc/src/edu/tamu/tcat/trc/search/SearchException.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,7 +18,7 @@ package edu.tamu.tcat.trc.search;
 /**
  * A base exception type for entry types to use within their search modules.
  */
-public class SearchException extends Exception
+public class SearchException extends RuntimeException
 {
    public SearchException()
    {
