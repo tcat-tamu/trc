1610cb6fc4987138fa27ae3d949dde1b6e986b40
Neal Audenaert <neala@tamu.edu>
2016-10-11 16:07:20 -0500

    Added see-also links to EntryFacade; added #remove method

--- bundles/edu.tamu.tcat.trc.impl.psql/src/edu/tamu/tcat/trc/impl/psql/EntryFacadeImpl.java
+++ bundles/edu.tamu.tcat.trc.impl.psql/src/edu/tamu/tcat/trc/impl/psql/EntryFacadeImpl.java
@@ -1,8 +1,11 @@
 package edu.tamu.tcat.trc.impl.psql;
 
 import static java.text.MessageFormat.format;
+import static java.util.stream.Collectors.toList;
 
 import java.util.Collection;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
 import edu.tamu.tcat.account.Account;
 import edu.tamu.tcat.trc.EntryFacade;
@@ -19,6 +22,8 @@ import edu.tamu.tcat.trc.services.bibref.repo.RefCollectionService;
 import edu.tamu.tcat.trc.services.notes.EditNoteCommand;
 import edu.tamu.tcat.trc.services.notes.Note;
 import edu.tamu.tcat.trc.services.notes.NotesService;
+import edu.tamu.tcat.trc.services.seealso.Link;
+import edu.tamu.tcat.trc.services.seealso.SeeAlsoService;
 
 public class EntryFacadeImpl<EntryType> implements EntryFacade<EntryType>
 {
@@ -30,8 +35,9 @@ public class EntryFacadeImpl<EntryType> implements EntryFacade<EntryType>
    private final EntryResolverRegistry resolvers;
    private final EntryResolver<Object> resolver;
 
-   private ReferenceServiceDelegate refSvcDelegate;
-   private EntryFacadeImpl<EntryType>.NoteServiceDelegate notesSvcDelegate;
+   private final ReferenceServiceDelegate refSvcDelegate;
+   private final NoteServiceDelegate notesSvcDelegate;
+   private final SeeAlsoSvcDelegate seeAlsoSvcDelegate;
 
    public EntryFacadeImpl(TrcServiceManager svcMgr, EntryRepositoryRegistry repoRegistry, EntryReference ref, Account acct, Class<EntryType> typeToken)
    {
@@ -55,6 +61,7 @@ public class EntryFacadeImpl<EntryType> implements EntryFacade<EntryType>
       // spin up delgates to manage specific services
       refSvcDelegate = new ReferenceServiceDelegate();
       notesSvcDelegate = new NoteServiceDelegate();
+      seeAlsoSvcDelegate = new SeeAlsoSvcDelegate();
    }
 
    @Override
@@ -105,6 +112,34 @@ public class EntryFacadeImpl<EntryType> implements EntryFacade<EntryType>
       return refSvcDelegate.editReferences();
    }
 
+   @Override
+   public Link addLink(EntryReference ref)
+   {
+      return seeAlsoSvcDelegate.link(ref);
+   }
+
+   @Override
+   public Collection<EntryReference> getLinks()
+   {
+      return seeAlsoSvcDelegate.getLinks();
+   }
+
+   @Override
+   public boolean removeLink(EntryReference ref)
+   {
+      return seeAlsoSvcDelegate.removeLink(ref);
+   }
+
+   @Override
+   public CompletableFuture<Void> remove()
+   {
+      return resolver.remove(account, ref)
+                 .thenAccept(flag -> seeAlsoSvcDelegate.remove())
+                 .thenAccept(flag -> notesSvcDelegate.remove())
+                 .thenAccept(flag -> refSvcDelegate.remove());
+   }
+
+
    private class ReferenceServiceDelegate
    {
       private final String token;
@@ -125,6 +160,11 @@ public class EntryFacadeImpl<EntryType> implements EntryFacade<EntryType>
       public EditBibliographyCommand editReferences()
       {
          return refSvc.edit(token);
+      }
+
+      public void remove()
+      {
+         refSvc.delete(token);
       }
    }
 
@@ -152,5 +192,50 @@ public class EntryFacadeImpl<EntryType> implements EntryFacade<EntryType>
       {
          return notesSvc.create();
       }
+
+      public void remove()
+      {
+         notesSvc.getNotes(ref).stream()
+               .forEach(note -> notesSvc.remove(note.getId()));
+      }
+   }
+
+   private class SeeAlsoSvcDelegate
+   {
+      private SeeAlsoService service;
+      private String source;
+
+      public SeeAlsoSvcDelegate()
+      {
+         service = svcMgr.getService(SeeAlsoService.makeContext(account));
+         source = Objects.requireNonNull(resolvers.tokenize(ref));
+      }
+
+      public Collection<EntryReference> getLinks()
+      {
+         return service.getFor(source).stream()
+               .map(link -> source.equals(link.getSource()) ? link.getTarget() : link.getSource())
+               .map(resolvers::decodeToken)
+               .collect(toList());
+      }
+
+      public Link link(EntryReference ref)
+      {
+         String target = resolvers.tokenize(ref);
+         return service.create(source, target);
+      }
+
+      public boolean removeLink(EntryReference ref)
+      {
+         String target = resolvers.tokenize(ref);
+         return service.delete(source, target);
+      }
+
+      public boolean remove()
+      {
+         return service.delete(source);
+      }
+
+
    }
 }
--- bundles/edu.tamu.tcat.trc/src/edu/tamu/tcat/trc/EntryFacade.java
+++ bundles/edu.tamu.tcat.trc/src/edu/tamu/tcat/trc/EntryFacade.java
@@ -1,12 +1,15 @@
 package edu.tamu.tcat.trc;
 
 import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
 
+import edu.tamu.tcat.trc.entries.core.repo.EntryRepository;
 import edu.tamu.tcat.trc.resolver.EntryReference;
 import edu.tamu.tcat.trc.services.bibref.ReferenceCollection;
 import edu.tamu.tcat.trc.services.bibref.repo.EditBibliographyCommand;
 import edu.tamu.tcat.trc.services.notes.EditNoteCommand;
 import edu.tamu.tcat.trc.services.notes.Note;
+import edu.tamu.tcat.trc.services.seealso.Link;
 
 /**
  * Provides a wrapper around a TRC Entry that can be used to link the entry
@@ -37,6 +40,7 @@ public interface EntryFacade<EntryType>
 
    /**
     * Create a new note that will be associated with this entry.
+    *
     * @return A command to edit the newly created note.
     */
    EditNoteCommand addNote();
@@ -45,11 +49,44 @@ public interface EntryFacade<EntryType>
 
    EditBibliographyCommand editReferences();
 
+   /**
+    * Adds a new 'See Also' link from this entry to the referenced entry.
+    *
+    * @param ref A reference to the entry to link to.
+    * @return The created link.
+    */
+   Link addLink(EntryReference ref);
+
+   /**
+    * Removes a 'See Also' link from this entry.
+    *
+    * @param ref The reference whose link should be deleted.
+    * @return Indicates whether the underlying data was changed. Will be <code>false</code>
+    *       if the referenced entry was not linked to this entry.
+    */
+   boolean removeLink(EntryReference ref);
+
+   /**
+    * @return A collection of entries that are linked to this one.
+    */
+   Collection<EntryReference> getLinks();     // TODO do we need to distinguish directionality?
+
+   /**
+    * Removes the referenced entry and all associated service content. In general, this method
+    * should be preferred to {@link EntryRepository#remove(String)} in order to ensure that
+    * linked resources are properly cleaned.
+    *
+    * @return A future that will pass through any exceptions encountered while
+    *       attempting to remove this entry.
+    */
+   CompletableFuture<Void> remove();
+
+
    // TODO versioning,
    //      meta information (removed, author, etc)
    //      ACL
    //      Tags
-   //      SeeAlso
    //      Publication
    //      Categorizations
+   //      TRC Relationships
 }
