48f8e925a05e4c8c6cc082b17bcf5c1baa4676ea
Neal Audenaert <neala@tamu.edu>
2016-11-03 09:45:58 -0500

    Clean up dead code in PeopleResourceService

--- bundles/edu.tamu.tcat.trc.entries.types.bio.rest/src/edu/tamu/tcat/trc/entries/types/bio/rest/PeopleResourceService.java
+++ bundles/edu.tamu.tcat.trc.entries.types.bio.rest/src/edu/tamu/tcat/trc/entries/types/bio/rest/PeopleResourceService.java
@@ -22,51 +22,20 @@ import java.util.logging.Logger;
 import javax.ws.rs.Path;
 
 import edu.tamu.tcat.trc.TrcApplication;
-import edu.tamu.tcat.trc.entries.types.bio.impl.search.BioSearchStrategy;
 import edu.tamu.tcat.trc.entries.types.bio.rest.v1.PeopleResource;
-import edu.tamu.tcat.trc.entries.types.bio.search.BioEntryQueryCommand;
-import edu.tamu.tcat.trc.search.solr.QueryService;
-import edu.tamu.tcat.trc.search.solr.SearchServiceManager;
 
 @Path("/")
 public class PeopleResourceService
 {
-
    private final static Logger logger = Logger.getLogger(PeopleResourceService.class.getName());
-   // TODO make this core application logic
 
-//   private EntryRepositoryRegistry registry;
-//   private ConfigurationProperties config;
-//   private TrcServiceManager serviceMgr;
-
-   private SearchServiceManager searchMgr;
-   private QueryService<BioEntryQueryCommand> queryService;
    private TrcApplication trcMgr;
-
-//   public void setConfig(ConfigurationProperties config)
-//   {
-//      this.config = config;
-//   }
-//
-//   public void setRepoRegistry(EntryRepositoryRegistry registry)
-//   {
-//      this.registry = registry;
-//   }
-//
-   public void setSearchSvcMgr(SearchServiceManager searchMgr)
-   {
-      this.searchMgr = searchMgr;
-   }
-//
-//   public void setServiceMgr(TrcServiceManager serviceMgr)
-//   {
-//      this.serviceMgr = serviceMgr;
-//   }
 
    public void setTrcApplication(TrcApplication trcMgr)
    {
       this.trcMgr = trcMgr;
    }
+
    // called by DS
    public void activate()
    {
@@ -74,29 +43,13 @@ public class PeopleResourceService
 
       try
       {
-         Objects.requireNonNull(searchMgr, "No search service configured");
-//         Objects.requireNonNull(serviceMgr, "No service manager configured");
-
-//         config = trcMgr.getConfig();
-//         serviceMgr = trcMgr.getServiceManager();
-//         registry = trcMgr.getEntryRepositoryManager();
-//         resolvers = trcMgr.getResolverRegistry();
-
-
-         BioSearchStrategy indexCfg = new BioSearchStrategy(trcMgr.getConfig());
-         queryService = searchMgr.getQueryService(indexCfg);
+         Objects.requireNonNull(trcMgr, "No search service configured");
       }
       catch (Exception ex)
       {
          logger.log(Level.SEVERE, "Failed to activate " + getClass().getSimpleName(), ex);
          throw ex;
       }
-   }
-
-   // called by DS
-   public void dispose()
-   {
-
    }
 
    // Would be better to handle token security here before delegating to the sub-resource,
